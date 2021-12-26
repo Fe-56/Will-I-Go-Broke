57817aec3615cd8094bb0f22c91b00a92cd90d72
Fe-56 <limfuoen1999@gmail.com>
2021-12-26 13:49:30 +0800

    Changing of file directories
    This is where I moved the commands_and_descriptions.txt and api_key.txt files into the folder called Data, and modified the test.py such that it can grab the contents from these .txt files in their new directories.

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
-with open ('commands_and_descriptions.txt') as commands_and_descriptions:
-    lines = commands_and_descriptions.readlines()
+# with open ('commands_and_descriptions.txt') as commands_and_descriptions:
+#     lines = commands_and_descriptions.readlines()
 
-print(lines)
+# print(lines)
 
-API_KEY = open('api_key.txt', 'r').read()
+# API_KEY = open('api_key.txt', 'r').read()
+# print(API_KEY)
+
+import os
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname, 'Data/api_key.txt')
+API_KEY = open(filename, 'r').read()
 print(API_KEY)
-
-print()
