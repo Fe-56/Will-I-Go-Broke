94fa9c9bc23cf6fd355835c1969eeeb2a7a33a0a
Fe-56 <limfuoen1999@gmail.com>
2021-12-28 11:12:02 +0800

    register_next_step_handler workings
    Added in example code from PyTelegramBotAPI GitHub repository into main.py and marked it out with comments to see and understand how the register_next_step_handler works

--- main.py
+++ main.py
@@ -41,13 +41,71 @@ def help(message):
 def disclaimer(message):
     bot.send_message(message.chat.id, "This bot does not:\n\n\n1. Collect your data or whatsoever\n\n2. This bot only serves to be a quick tool for you to input your finances over your university/school life so you can better visualise them and is not a full-fledged financial planner\n\n3. This bot only takes into account of controllable finances that you input yourself, and any market fluctuations in the economy will not be accounted for")
 
-# /plan
-@bot.message_handler(commands = ['plan', 'Plan'])
-def plan(message):
-    user = User(message.from_user.username) # cretes a User class with the Telegram username as the name attribute
-    markup = types.ForceReply(selective = False) # for a ForceReply
-    sent_message = bot.send_message(message.chat.id, "What is your current bank balance?", reply_markup = markup) # ForceReply
-    bot.register_next_step_handler(sent_message, plan.)
+'''The following is just to understand how the register_next_step_handler works'''
+user_dict = {}
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    msg = bot.reply_to(message, """\
+Hi there, I am Example bot.
+What's your name?
+""")
+    bot.register_next_step_handler(msg, process_name_step)
+
+
+def process_name_step(message):
+    try:
+        chat_id = message.chat.id
+        name = message.text
+        user = User(name)
+        user_dict[chat_id] = user
+        msg = bot.reply_to(message, 'How old are you?')
+        bot.register_next_step_handler(msg, process_age_step)
+    except Exception as e:
+        bot.reply_to(message, 'oooops')
+
+
+def process_age_step(message):
+    try:
+        chat_id = message.chat.id
+        age = message.text
+        if not age.isdigit():
+            msg = bot.reply_to(message, 'Age should be a number. How old are you?')
+            bot.register_next_step_handler(msg, process_age_step)
+            return
+        user = user_dict[chat_id]
+        user.age = age
+        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+        markup.add('Male', 'Female')
+        msg = bot.reply_to(message, 'What is your gender', reply_markup=markup)
+        bot.register_next_step_handler(msg, process_sex_step)
+    except Exception as e:
+        bot.reply_to(message, 'oooops')
+
+
+def process_sex_step(message):
+    try:
+        chat_id = message.chat.id
+        sex = message.text
+        user = user_dict[chat_id]
+        if (sex == u'Male') or (sex == u'Female'):
+            user.sex = sex
+        else:
+            raise Exception("Unknown sex")
+        bot.send_message(chat_id, 'Nice to meet you ' + user.name + '\n Age:' + str(user.age) + '\n Sex:' + user.sex)
+    except Exception as e:
+        bot.reply_to(message, 'oooops')
+
+'''End of the register_next_step_handler'''
+
+# # /plan
+# @bot.message_handler(commands = ['plan', 'Plan'])
+# def plan(message):
+#     user = User(message.from_user.username) # cretes a User class with the Telegram username as the name attribute
+#     markup = types.ForceReply(selective = False) # for a ForceReply
+#     sent_message = bot.send_message(message.chat.id, "What is your current bank balance?", reply_markup = markup) # ForceReply
+#     bot.register_next_step_handler(sent_message, plan.)
 
 # @bot.message_handler(content_types=['text'])
 # def welcome(pm):
